4cf747e5c0df4ee597f64584cf2d06304560e4bb
Megan Chen <mcgauss1181@gmail.com>
2014-10-18 11:41:17 -0400

    Delete unnecessary node.js file that didn't work earlier

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
